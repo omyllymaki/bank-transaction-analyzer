7e7a8a03fa4d99c51b20f5dcb8ecc839b88400d4
Ossi Myllymäki <omyllymaki@gmail.com>
2021-01-10 11:08:50 +0200

    Update default config and readme

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ Account Analyzer provides analysis and visualization of Nordea internet bank dat
 * Start GUI program from command line by typing python main.py.
 * Load data with load button which opens file dialog where you can choose multiple files for analysis.
 * Set filter values and analyze you data by pressing Analyze data; this updates the figures.
+* Change tabs to see different views and analyses for your data.
 
 ## Example (with simulated data)
 
--- config.py
+++ config.py
@@ -1,10 +1,19 @@
 # Data to be rejected as dict of lists where keys are columns and values are rows
 # E.g. DROP_DATA = {'a': ['x', 'y']} rejects all rows that have values 'x' or 'y' in column 'a'
 DROP_DATA = {
-    'Saaja/Maksaja': ['x', 'y'],
-    'Tapahtuma': ['z']
+    'target': ['x', 'y'],
+    'event': ['z']
 }
 
 # Default dir that will opened when data is loaded
 # Current script folder will be used if None
 DEFAULT_DATA_DIR = None
+
+# Map of indicators that we want to follow
+# Key is indicator name
+# Value is regexp pattern used to filter all the events with matching criteria
+INDICATORS = {
+    "VR": "vr",
+    "Store": "sale|prisma|citymarket|k market|s market|k supermarket|kylävalinta|lidl",
+    "All": ".*"
+}
