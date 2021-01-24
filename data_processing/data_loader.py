383e116dd1269eac2f27b632a2ae653932b14b3d
Ossi Myllymäki <omyllymaki@gmail.com>
2021-01-24 14:37:38 +0200

    Read account number from file, not from file name

--- data_processing/data_loader.py
+++ data_processing/data_loader.py
@@ -10,15 +10,7 @@ class DataLoader:
         raw_data_list = []
         for path in file_paths:
             df = pd.read_csv(path, sep='\t', header=1)
-            df['account_number'] = self._extract_account_name_from_file_path(path)
+            df["account_number"] = pd.read_csv(path, sep='\t', nrows=1, header=None)[1].values[0]
             raw_data_list.append(df)
         data = pd.concat(raw_data_list)
         return data
-
-    @staticmethod
-    def _extract_account_name_from_file_path(file_path: str) -> str:
-        try:
-            account_name = re.findall('FI\d+', file_path)[0]
-        except:
-            account_name = None
-        return account_name
