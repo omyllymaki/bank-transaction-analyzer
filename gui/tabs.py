8d000a7aa86cd5f3a49a99aae2e92916470d4ef3
Ossi Myllymäki <omyllymaki@gmail.com>
2021-01-30 14:38:45 +0200

    Sort event table by column when header is clicked

--- gui/tabs.py
+++ gui/tabs.py
@@ -272,9 +272,14 @@ class EventTableTab(QTabWidget):
         self.table_view = QtWidgets.QTableView()
         self.table_view.setObjectName("tableView")
         self.group_by_target_button = QRadioButton("Group by target")
+        self.header = self.table_view.horizontalHeader()
         self._set_layout()
         self._set_connections()
         self.group_by_target = False
+        self.data = None
+        self.grouped_data = None
+        self.sort_col_index = 0
+        self.ascending = True
 
     def _set_layout(self):
         self.layout = QVBoxLayout()
@@ -284,26 +289,47 @@ class EventTableTab(QTabWidget):
 
     def _set_connections(self):
         self.group_by_target_button.toggled.connect(self._handle_grouping_button_toggle)
+        self.header.sectionClicked.connect(self._handle_header_clicked)
 
     def _handle_grouping_button_toggle(self):
         if self.group_by_target_button.isChecked():
             self.group_by_target = True
         else:
             self.group_by_target = False
+        self._render_table()
+
+    def _handle_header_clicked(self, index):
+        self.sort_col_index = index
+        order = self.header.sortIndicatorOrder()
+        if order == 0:
+            self.ascending = True
+        else:
+            self.ascending = False
+        self._render_table()
 
     def show_data(self, data):
-        if self.group_by_target:
-            grouped = data.groupby("target", as_index=False)
-            table_data = pd.DataFrame()
-            table_data[["target", "sum"]] = grouped.sum()[["target", "value"]].sort_values("value")
-            table_data["average"] = grouped.mean()["value"]
-            table_data["min"] = grouped.min()["value"]
-            table_data["max"] = grouped.max()["value"]
-            table_data["count"] = grouped.count()["value"]
-            model = DataFrameModel(table_data)
-        else:
-            model = DataFrameModel(data)
-        self.table_view.setModel(model)
+        self.data = data
+        grouped = self.data.groupby("target", as_index=False)
+        grouped_data = pd.DataFrame()
+        grouped_data[["target", "sum"]] = grouped.sum()[["target", "value"]]
+        grouped_data["average"] = grouped.mean()["value"]
+        grouped_data["min"] = grouped.min()["value"]
+        grouped_data["max"] = grouped.max()["value"]
+        grouped_data["count"] = grouped.count()["value"]
+        self.grouped_data = grouped_data
+
+        self._render_table()
+
+    def _render_table(self):
+        if self.data is not None:
+            if self.group_by_target:
+                table_data = self.grouped_data
+            else:
+                table_data = self.data
+            sort_col_name = table_data.columns[self.sort_col_index]
+            table_data_sorted = table_data.sort_values(sort_col_name, ascending=self.ascending)
+            model = DataFrameModel(table_data_sorted)
+            self.table_view.setModel(model)
 
 
 class StackedBarsTab(QTabWidget):
