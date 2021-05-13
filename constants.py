ce61201d09aba49d03bd295388acd10b3c6cbbeb
Ossi Myllymäki <omyllymaki@gmail.com>
2021-05-13 17:12:57 +0300

    Add sw version

---
+++ constants.py
@@ -0,0 +1,1 @@
+SW_VERSION = 0.1
--- gui/main_window.py
+++ gui/main_window.py
@@ -1,12 +1,13 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow
 
+from constants import SW_VERSION
 from gui.main_view import MainView
 
 
 class MainWindow(QMainWindow):
     def __init__(self, config):
         super().__init__(flags=Qt.Window)
-        self.setWindowTitle('Bank Transaction Analyzer')
+        self.setWindowTitle(f'Bank Transaction Analyzer v{SW_VERSION}')
         self.main_view = MainView(config)
         self.setCentralWidget(self.main_view)
