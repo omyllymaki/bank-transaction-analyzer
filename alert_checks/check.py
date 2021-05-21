c12896880273a0ea36abba86a9eb5b46ea0da70f
Ossi Myllymäki <omyllymaki@gmail.com>
2021-05-21 18:24:26 +0300

    Update alert checking

--- alert_checks/check.py
+++ alert_checks/check.py
@@ -1,43 +1,51 @@
 import logging
 from abc import abstractmethod
 from collections import Iterable
-from typing import List, Union
+from typing import Tuple, Union
 
-import numpy as np
 import pandas as pd
+import numpy as np
 
 logger = logging.getLogger(__name__)
+
+Values = Union[list, float, int, np.ndarray]
 
 
 class Check:
 
-    def __init__(self, reference_value, name=None):
-        self.ref_val = reference_value
+    def __init__(self, name=None):
         if name is None:
             self.name = self.get_class_name()
 
     @abstractmethod
-    def calculate_values(self, data: pd.DataFrame) -> Union[List[float], float, np.ndarray, pd.Series]:
+    def calculate_values(self, data: pd.DataFrame) -> Tuple[Values, Values]:
         raise NotImplementedError
 
     @abstractmethod
-    def is_ok(self, value) -> bool:
+    def is_ok(self, test_value, ref_value) -> bool:
         raise NotImplementedError
 
-    def on_fail(self, value):
-        logger.error(f"Check {self.name} doesn't pass: calculated {value}; reference {self.ref_val}")
+    def on_fail(self, value, ref_val):
+        logger.error(f"Check {self.name} doesn't pass: calculated {value}; reference {ref_val}")
 
     def run(self, data: pd.DataFrame):
-        values = self.calculate_values(data)
-        if not isinstance(values, Iterable):
-            values = [values]
+        self.calculate_values(data)
+        test_values, ref_values = self.calculate_values(data)
+
+        if not isinstance(test_values, Iterable):
+            test_values = [test_values]
+        if not isinstance(ref_values, Iterable):
+            ref_values = [ref_values for _ in range(len(test_values))]
+
+        if len(test_values) != len(ref_values):
+            raise Exception("Number of calculated values doesn't match with number of reference values")
 
         all_values_ok = True
-        for value in values:
-            is_ok = self.is_ok(value)
-            logger.debug(f"Check: {is_ok}, {value}, {self.ref_val}")
+        for value, ref_value in zip(test_values, ref_values):
+            is_ok = self.is_ok(value, ref_value)
+            logger.debug(f"Test: {is_ok}, {value}, {ref_value}")
             if not is_ok:
-                self.on_fail(value)
+                self.on_fail(value, ref_value)
                 all_values_ok = False
 
         return all_values_ok
--- alert_checks/checker.py
+++ alert_checks/checker.py
@@ -15,11 +15,14 @@ class Checker:
         n_checks = len(checks)
         n_passed = 0
         all_ok = True
+        not_passed = []
         for i, check in enumerate(checks, 1):
             logger.info(f"Performing check {i}/{n_checks}: {check.name}")
             if check.run(data):
                 n_passed += 1
             else:
                 all_ok = False
+                not_passed.append(check.name)
         logger.info(f"Alert checks done: {n_passed}/{n_checks} ({100 * n_passed / n_checks:0.1f} %) passed")
+        logger.info(f"Checks that didn't pass: {not_passed}")
         return all_ok
