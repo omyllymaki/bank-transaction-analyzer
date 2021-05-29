e52a24b383b2fa5bc5483425dfd9feeca44a0c48
Ossi Myllymäki <omyllymaki@gmail.com>
2021-05-29 11:24:04 +0300

    Event tab table updates: group by any column by right clicking column name

--- src/gui/tabs/event_table.py
+++ src/gui/tabs/event_table.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtCore import Qt, pyqtSignal
-from PyQt5.QtWidgets import QRadioButton, QVBoxLayout, QMenu, QAction
+from PyQt5.QtWidgets import QRadioButton, QVBoxLayout, QMenu, QAction, QAbstractItemView
 
 from src.gui.dataframe_model import DataFrameModel
 from src.gui.dialog_boxes import show_warning
@@ -19,9 +19,9 @@ class EventTableTab(BaseTab):
         self.config = config
         self.table_view = QtWidgets.QTableView()
         self.table_view.setObjectName("tableView")
-        self.group_by_target_button = QRadioButton("Group by target")
         self.header = self.table_view.horizontalHeader()
         self.group_by_target = False
+        self.group_by = "target"
         self.data = None
         self.table_data_sorted = None
         self.grouped_data = None
@@ -32,19 +32,21 @@ class EventTableTab(BaseTab):
 
     def _set_layout(self):
         self.layout = QVBoxLayout()
-        self.layout.addWidget(self.group_by_target_button)
         self.layout.addWidget(self.table_view)
         self.setLayout(self.layout)
 
     def _set_connections(self):
-        self.group_by_target_button.toggled.connect(self._handle_grouping_button_toggle)
         self.header.sectionClicked.connect(self._handle_header_clicked)
+        self.header.sectionClicked.connect(self._handle_header_clicked)
+        self.header.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.header.customContextMenuRequested.connect(self._handle_header_right_clicked)
+        self.header.setSelectionMode(QAbstractItemView.SingleSelection)
 
-    def _handle_grouping_button_toggle(self):
-        if self.group_by_target_button.isChecked():
-            self.group_by_target = True
-        else:
-            self.group_by_target = False
+    def _handle_header_right_clicked(self, position):
+        index = self.header.logicalIndexAt(position)
+        self.group_by = self.table_data_sorted.columns[index]
+        self.group_by_target = not self.group_by_target
+        self._group_data()
         self.sort_col_index = 0
         self._render_table()
 
@@ -59,17 +61,22 @@ class EventTableTab(BaseTab):
 
     def handle_data(self, data):
         self.data = data[self.show_columns]
-        grouped = self.data.groupby("target", as_index=False)
+        self._group_data()
+        self._render_table()
+
+    def _group_data(self):
+        if not self.group_by_target:
+            return
+        grouped = self.data.groupby(self.group_by, as_index=False)
         grouped_data = pd.DataFrame()
-        grouped_data[["target", "sum"]] = grouped.sum()[["target", "value"]]
+        grouped_data[self.group_by] = grouped.first()[self.group_by]
+        grouped_data["sum"] = grouped.sum()["value"]
         grouped_data["mean"] = grouped.mean()["value"]
         grouped_data["median"] = grouped.median()["value"]
         grouped_data["min"] = grouped.min()["value"]
         grouped_data["max"] = grouped.max()["value"]
         grouped_data["count"] = grouped.count()["value"]
         self.grouped_data = grouped_data
-
-        self._render_table()
 
     def _render_table(self):
         if self.data is not None:
