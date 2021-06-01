45256126b3a09f622e12d79ac72f0212eacec226
Ossi Myllymäki <omyllymaki@gmail.com>
2021-06-01 17:55:57 +0300

    Add warning log message if data contains duplicate rows

--- src/data_processing/prepare_data.py
+++ src/data_processing/prepare_data.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List, Dict
 
 import pandas as pd
@@ -5,6 +6,8 @@ import pandas as pd
 from src.data_processing.data_filtering import DataFilter
 from src.data_processing.data_preprocessor import DataPreprocessor
 from src.data_processing.validation import validate
+
+logger = logging.getLogger(__name__)
 
 
 def prepare_data(file_paths: List[str],
@@ -15,6 +18,11 @@ def prepare_data(file_paths: List[str],
     data_preprocessor = DataPreprocessor()
     data_filter = DataFilter()
     raw_data = data_loader.load(file_paths)
+    duplicates = raw_data[raw_data.duplicated(keep=False)]
+    n_duplicates = duplicates.shape[0]
+    if n_duplicates > 0:
+        logger.warning(
+            f"Warning: loaded data contains {n_duplicates} duplicate rows. Duplicate rows:\n {duplicates.to_markdown()}")
     transformed_data = data_transformer.transform(raw_data)
     validate(transformed_data)
     preprocessed_data = data_preprocessor.process(transformed_data)
