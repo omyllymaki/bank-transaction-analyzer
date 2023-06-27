53ebbe29ebc1a6aef4048cecf0e01effacef7cc5
Ossi Myllymäki <omyllymaki@gmail.com>
2023-06-27 15:54:43 +0300

    Update sw version

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-SW_VERSION = 0.4
+SW_VERSION = 0.5
 COLUMNS = ["target", "account_number", "value", "time", "message", "event", "category", "labels", "bank", "id", "notes"]
 EDITABLE_COLUMNS = ["notes"]
