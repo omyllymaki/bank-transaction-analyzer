2a9d2c2ad607450246d12bd180085d3de8e75ce8
Ossi Myllymäki <omyllymaki@gmail.com>
2023-06-29 17:40:05 +0300

    Remove background colors from tables

--- src/gui/dataframe_model.py
+++ src/gui/dataframe_model.py
@@ -11,8 +11,6 @@ class DataFrameModel(QtCore.QAbstractTableModel):
     """
 
     data_edited_signal = pyqtSignal(tuple)
-    colors_green = ["#f7fcf5", "#e5f5e0", "#c7e9c0", "#a1d99b", "#74c476", "#41ab5d", "#238b45", "#006d2c", "#00441b"]
-    colors_red = ["#fff5eb", "#fee6ce", "#fdd0a2", "#fdae6b", "#fd8d3c", "#f16913", "#d94801", "#a63603", "#7f2704"]
 
     def __init__(self, data, parent=None, columns_for_edition=None):
         QtCore.QAbstractTableModel.__init__(self, parent)
@@ -34,24 +32,6 @@ class DataFrameModel(QtCore.QAbstractTableModel):
         if index.isValid():
             if role == QtCore.Qt.DisplayRole:
                 return str(self._data.iloc[index.row(), index.column()])
-            if role == QtCore.Qt.BackgroundRole:
-
-                value = self._data.iloc[index.row()][index.column()]
-                is_number = isinstance(value, int) or isinstance(value, float)
-
-                if is_number:
-                    if value < 0:
-                        min_value = self._min_values[index.column()]
-                        ratio_sqrt = np.sqrt(abs(value) / abs(min_value))
-                        color_index = math.ceil(len(self.colors_green) * ratio_sqrt) - 1
-                        color = self.colors_red[color_index]
-                    else:
-                        max_value = self._max_values[index.column()]
-                        ratio_sqrt = np.sqrt(value / max_value)
-                        color_index = math.ceil(len(self.colors_green) * ratio_sqrt) - 1
-                        color = self.colors_green[color_index]
-                    return QtGui.QColor(color)
-
         return None
 
     def headerData(self, col, orientation, role):
