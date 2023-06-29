f3e8dcf594561880e6dcf220e4763d034fd544c5
Ossi Myllymäki <omyllymaki@gmail.com>
2023-06-29 20:52:21 +0300

    Remove sw version

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,13 +1,12 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow
 
-from constants import SW_VERSION
 from src.gui.main_view import MainView
 
 
 class MainWindow(QMainWindow):
     def __init__(self, config_manager):
         super().__init__(flags=Qt.Window)
-        self.setWindowTitle(f'Bank Transaction Analyzer v{SW_VERSION}')
+        self.setWindowTitle(f'Bank Transaction Analyzer')
         self.main_view = MainView(config_manager)
         self.setCentralWidget(self.main_view)
