a27bb9df7f1f651e89af90957ed05131b8c72ce5
Ossi Myllymäki <omyllymaki@gmail.com>
2023-08-30 23:55:35 +0300

    Remove create app script

--- create_app.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "Creating application"
-pyinstaller main.py --clean --onefile --distpath bank_transaction_analyzer_app  --name bank-transaction-analyzer-app
-cp config.json ./bank_transaction_analyzer_app/
-cp -r test_data ./bank_transaction_analyzer_app/
-cp README.md ./bank_transaction_analyzer_app/
-zip -r bank_transaction_analyzer_app.zip bank_transaction_analyzer_app/
-
