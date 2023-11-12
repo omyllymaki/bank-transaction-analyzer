c23a44bf9fe9fe5cf5071551f87e974a75362cdd
Ossi Myllymäki <omyllymaki@gmail.com>
2023-11-12 11:39:41 +0200

    Hande empty time values

--- src/data_processing/transformers/nordea_transformer.py
+++ src/data_processing/transformers/nordea_transformer.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import numpy as np
 import pandas as pd
 
 from src.data_processing.transformers.transformer_interface import TransformerInterface
@@ -20,14 +21,17 @@ class NordeaTransformer(TransformerInterface):
         for col_target, col_source in self.mapping.items():
             transformed_data[col_target] = data[col_source]
         transformed_data["value"] = transformed_data["value"].apply(self._convert_string_to_float)
+        transformed_data["time"] = transformed_data["time"].astype(str)
         transformed_data["time"] = transformed_data["time"].apply(self._convert_string_to_datetime)
         transformed_data["bank"] = "Nordea (old format)"
         return transformed_data
 
     @staticmethod
     def _convert_string_to_datetime(date_str: str) -> datetime:
-        date_datetime = datetime.strptime(date_str, '%d.%m.%Y')
-        return date_datetime
+        try:
+            return datetime.strptime(date_str, '%d.%m.%Y')
+        except ValueError:
+            return None
 
     @staticmethod
     def _convert_string_to_float(value: str) -> float:
