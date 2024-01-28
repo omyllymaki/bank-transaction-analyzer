e60cdbfc0bf4180247d8d33d956f8b9212cb6d57
Ossi Myllymäki <omyllymaki@gmail.com>
2024-01-28 17:33:51 +0200

    Add profiling option

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import argparse
+import cProfile
+import logging
+import pstats
 import sys
+import os
 
 import pandas as pd
 from PyQt5.QtWidgets import QApplication
@@ -9,6 +13,8 @@ from src.gui.main_window import MainWindow
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
+PROFILE = True
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -16,11 +22,24 @@ def main():
     args = parser.parse_args()
     print(f"Reading configuration from {args.config}")
     config_manager = ConfigManager(args.config)
+    profiler = cProfile.Profile()
+
+    if PROFILE:
+        profiler.enable()
 
     app = QApplication(sys.argv)
     mw = MainWindow(config_manager)
     mw.show()
     app.exec()
+
+    if PROFILE:
+        profiler.disable()
+        profiler.dump_stats("profile_data")
+        stats = pstats.Stats("profile_data")
+        stats.sort_stats("cumulative")
+        stats.print_stats()
+        stats.dump_stats("profile_data.stats")
+        os.system("snakeviz profile_data.stats")
 
 
 if __name__ == "__main__":
